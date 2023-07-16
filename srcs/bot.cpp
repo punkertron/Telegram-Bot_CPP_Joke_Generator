6be366275bd6c98cb6d59b8674b8f1e99e48fbcf
punkertron <vascherbatvo@mail.ru>
2023-07-16 22:44:42 +0400

    change emoji and sticker response logic

--- srcs/bot.cpp
+++ srcs/bot.cpp
@@ -8,6 +8,33 @@
 #include "utils.hpp"
 #include "request_response.hpp"
 
+
+// https://apps.timwhitlock.info/emoji/tables/unicode
+
+static const std::vector<std::string> Emodjis = {
+	"\xF0\x9F\x98\x81",
+	"\xF0\x9F\x98\x82",
+	"\xF0\x9F\x98\x83",
+	"\xF0\x9F\x98\x84",
+	"\xF0\x9F\x98\x85",
+	"\xF0\x9F\x98\x86",
+	"\xF0\x9F\x98\x9C"
+};
+
+static const std::vector<std::string> Stickers = {
+	"CAACAgIAAxkBAAEJtLBks8ksyTXO3Ix0TWXqTm7rc5Z5IgAChgMAAkcVaAl9En4PCvCBli8E",
+	"CAACAgIAAxkBAAEJtLJks8kwT28BiSPKQ-DW9oH5RBHvowACRgMAAm2wQgMPoPbV7P8zDi8E",
+	"CAACAgQAAxkBAAEJtLRks8kyLxeO7SI0wruuxTxT7d8R0AACCQAD133WJREiMki6J6qGLwQ",
+	"CAACAgIAAxkBAAEJtLZks8k1cImcOnQssaM4ozk4OWXVCAACXQADKuBSG0Y9HMuI3J2ELwQ",
+	"CAACAgQAAxkBAAEJtLhks8k46Qy3wvEGBWuvKngULGbgdwACMgEAAqghIQZ4LmzPtHHGZC8E",
+	"CAACAgIAAxkBAAEJtLpks8k7OIPir4W5klqy5jgRUmprpwACRgADUomRI_j-5eQK1QodLwQ",
+	"CAACAgIAAxkBAAEJtLxks8k9VBC4vFHIM80nCrOapuHZUAACdAMAAvoLtgjV5oYGGtDaUC8E",
+	"CAACAgIAAxkBAAEJtL5ks8lAuP8ySxKIz3KqU5Ha3dVccgACvg8AAvbk0EmejJ6Vjfwi8i8E",
+	"CAACAgIAAxkBAAEJtMBks8lDbMxp8k9wTc-n4ieBwyFFzwACzxkAAiUBwEh5PNDrZsRPSS8E"
+};
+
+
+
 tgbot::tgbot(const std::string &api_key):
 	m_bot(api_key), m_longPoll(m_bot)
 {
@@ -24,19 +51,15 @@ tgbot::tgbot(const std::string &api_key):
 	m_bot.getApi().setMyCommands(commands);
 }
 
-static const std::string random_smile()
+static void random_smile(const TgBot::Api& api, const int64_t chat_id)
 {
 	srand(time(NULL));
 
-	std::vector<std::string> v = {"\xF0\x9F\x98\x81"
-								, "\xF0\x9F\x98\x82"
-								, "\xF0\x9F\x98\x83"
-								, "\xF0\x9F\x98\x84"
-								, "\xF0\x9F\x98\x85"
-								, "\xF0\x9F\x98\x86"
-								, "\xF0\x9F\x98\x9C"};
-
-	return std::move(v[rand() % v.size()]);
+	int r = rand();
+	if (r % 3 == 1)
+		api.sendMessage(chat_id, Emodjis[r % Emodjis.size()]);
+	else if (r % 3 == 2)
+		api.sendSticker(chat_id, Stickers[r % Stickers.size()]);
 }
 
 
@@ -71,8 +94,8 @@ void tgbot::run()
 				if (res.getType() == "single")
 				{
 					m_bot.getApi().sendMessage(message->chat->id, res.getJoke());
-					std::this_thread::sleep_for(std::chrono::seconds(2));
-					m_bot.getApi().sendMessage(message->chat->id, random_smile());
+					std::this_thread::sleep_for(std::chrono::seconds(4));
+					random_smile(m_bot.getApi(), message->chat->id);
 				}
 				else
 				{
@@ -80,7 +103,7 @@ void tgbot::run()
 					std::this_thread::sleep_for(std::chrono::seconds(4));
 					m_bot.getApi().sendMessage(message->chat->id, res.getDelivery());
 					std::this_thread::sleep_for(std::chrono::seconds(2));
-					m_bot.getApi().sendMessage(message->chat->id, random_smile());
+					random_smile(m_bot.getApi(), message->chat->id);
 				}
 			}
 		}
